dc94a8c3f270207747dd5e3fd631ce58c1f43121
AntMur00 <zaitg3r@gmail.com>
2023-10-05 14:33:28 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace SMOLab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.epsilonTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.halfDivisionButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.deltaTB = new System.Windows.Forms.TextBox();
+            this.goldenRatioButton = new System.Windows.Forms.Button();
+            this.FibonacciButton = new System.Windows.Forms.Button();
+            this.NewtonButton = new System.Windows.Forms.Button();
+            this.outputTB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // epsilonTB
+            // 
+            this.epsilonTB.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.epsilonTB.Location = new System.Drawing.Point(243, 10);
+            this.epsilonTB.Name = "epsilonTB";
+            this.epsilonTB.Size = new System.Drawing.Size(100, 23);
+            this.epsilonTB.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Function: x^3-x; 0<x<1";
+            // 
+            // halfDivisionButton
+            // 
+            this.halfDivisionButton.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.halfDivisionButton.Location = new System.Drawing.Point(12, 47);
+            this.halfDivisionButton.Name = "halfDivisionButton";
+            this.halfDivisionButton.Size = new System.Drawing.Size(91, 45);
+            this.halfDivisionButton.TabIndex = 3;
+            this.halfDivisionButton.Text = "Половинное деление";
+            this.halfDivisionButton.UseVisualStyleBackColor = true;
+            this.halfDivisionButton.Click += new System.EventHandler(this.halfDivisionButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(181, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Epsilon";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(349, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Delta";
+            // 
+            // deltaTB
+            // 
+            this.deltaTB.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.deltaTB.Location = new System.Drawing.Point(397, 10);
+            this.deltaTB.Name = "deltaTB";
+            this.deltaTB.Size = new System.Drawing.Size(100, 23);
+            this.deltaTB.TabIndex = 2;
+            // 
+            // goldenRatioButton
+            // 
+            this.goldenRatioButton.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.goldenRatioButton.Location = new System.Drawing.Point(109, 47);
+            this.goldenRatioButton.Name = "goldenRatioButton";
+            this.goldenRatioButton.Size = new System.Drawing.Size(91, 45);
+            this.goldenRatioButton.TabIndex = 4;
+            this.goldenRatioButton.Text = "Золотое сечение";
+            this.goldenRatioButton.UseVisualStyleBackColor = true;
+            this.goldenRatioButton.Click += new System.EventHandler(this.goldenRatioButton_Click);
+            // 
+            // FibonacciButton
+            // 
+            this.FibonacciButton.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FibonacciButton.Location = new System.Drawing.Point(206, 47);
+            this.FibonacciButton.Name = "FibonacciButton";
+            this.FibonacciButton.Size = new System.Drawing.Size(91, 45);
+            this.FibonacciButton.TabIndex = 5;
+            this.FibonacciButton.Text = "Метод Фибоначчи";
+            this.FibonacciButton.UseVisualStyleBackColor = true;
+            this.FibonacciButton.Click += new System.EventHandler(this.FibonacciButton_Click);
+            // 
+            // NewtonButton
+            // 
+            this.NewtonButton.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NewtonButton.Location = new System.Drawing.Point(303, 47);
+            this.NewtonButton.Name = "NewtonButton";
+            this.NewtonButton.Size = new System.Drawing.Size(91, 45);
+            this.NewtonButton.TabIndex = 6;
+            this.NewtonButton.Text = "Метод Ньютона";
+            this.NewtonButton.UseVisualStyleBackColor = true;
+            this.NewtonButton.Click += new System.EventHandler(this.NewtonButton_Click);
+            // 
+            // outputTB
+            // 
+            this.outputTB.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.outputTB.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.outputTB.Location = new System.Drawing.Point(0, 121);
+            this.outputTB.Multiline = true;
+            this.outputTB.Name = "outputTB";
+            this.outputTB.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.outputTB.Size = new System.Drawing.Size(587, 193);
+            this.outputTB.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 314);
+            this.Controls.Add(this.outputTB);
+            this.Controls.Add(this.NewtonButton);
+            this.Controls.Add(this.FibonacciButton);
+            this.Controls.Add(this.goldenRatioButton);
+            this.Controls.Add(this.deltaTB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.halfDivisionButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.epsilonTB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox epsilonTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button halfDivisionButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox deltaTB;
+        private System.Windows.Forms.Button goldenRatioButton;
+        private System.Windows.Forms.Button FibonacciButton;
+        private System.Windows.Forms.Button NewtonButton;
+        private System.Windows.Forms.TextBox outputTB;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static SMOLab1.Form1;
+
+namespace SMOLab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        float epsilon;
+        float delta;
+        public float a;
+        public float b;
+        public float alpha;
+        public float beta;            
+        public float xStar;
+        public float x;
+        public float xPrev;
+        static readonly float lambdaGR = (float)(Math.Sqrt(5) - 1) / 2;
+
+        #region Function Description
+        /// <summary>
+        /// <paramref name="x"/>^3-<paramref name="x"/>
+        /// </summary>
+        /// <param name="x"></param>
+        private float MainFunction(float x) => (x * x * x) - x;
+
+        /// <summary>
+        /// 1-ая Производная от <paramref name="x"/>^3-<paramref name="x"/>
+        /// </summary>
+        /// <param name="x"></param>
+        private float MainFuncFirDerivative(float x) => 3 * (x * x) - 1;
+
+        /// <summary>
+        /// 2-ая Производная от <paramref name="x"/>^3-<paramref name="x"/>
+        /// </summary>
+        /// <param name="x"></param>
+        private float MainFuncSecDerivative(float x) => 6 * x;
+
+        private int Fibonacci(int n)
+        {
+            int prev = 0;
+            int current = 1;
+            int temp;
+
+            Console.WriteLine(prev);
+            for (int i = 0; i <= n; i++)
+            {
+                Console.WriteLine(current);
+                temp = current;
+                current = current + prev;
+                prev = temp;
+            }
+            return current; }
+        #endregion
+
+        #region Check
+        private int CorrectCheck(bool notDelta)
+        {
+            if (!float.TryParse(epsilonTB.Text, out epsilon))
+            {
+                MessageBox.Show("Введите Epsilon!");
+                return -1;
+            }
+            if (notDelta == true)
+                return 1;
+            if (!float.TryParse(deltaTB.Text, out delta))
+            {
+                MessageBox.Show("Введите Delta!");
+                return -1;
+            }
+            return 1;
+        }
+        #endregion
+
+        #region Solving Methods
+        private Task HalfDivision()
+        {
+            a = 0;
+            b = 1;
+            int k = 0;
+
+            while (/*!((b - a) < epsilon)*/k != 10)
+            {
+                k++;
+                outputTB.Text += $"Iteration {k}:" + Environment.NewLine;
+                float temp1 = (float)(a + b) / 2;
+
+                alpha = temp1 - delta;
+                beta = temp1 + delta;
+
+                outputTB.Text += $"alpha{k} = {alpha}" + Environment.NewLine;
+                outputTB.Text += $"beta{k} = {beta}" + Environment.NewLine;
+
+                float mfAlpha = MainFunction(alpha);
+                float mfBeta = MainFunction(beta);
+
+                if (mfAlpha > mfBeta)
+                    a = alpha;
+                else
+                    b = beta;
+
+                outputTB.Text += $"[{a};{b}]" + Environment.NewLine;
+                outputTB.Text += Environment.NewLine;
+            }
+
+            xStar = (a + b) / 2;
+            outputTB.Text += $"~x* = {xStar}" + Environment.NewLine;
+            outputTB.Text += Environment.NewLine;
+            return Task.CompletedTask;
+        }
+
+        private Task GoldenRatio()
+        {
+            a = 0;
+            b = 1;
+            int k = 1;
+
+            outputTB.Text += $"Iteration 1:" + Environment.NewLine;
+
+            alpha = a + (1 - lambdaGR) * (b - a);
+            beta = a + lambdaGR * (b - a);
+
+            outputTB.Text += $"alpha{k} = {alpha}" + Environment.NewLine;
+            outputTB.Text += $"beta{k} = {beta}" + Environment.NewLine;
+            outputTB.Text += $"[{a};{b}]" + Environment.NewLine;
+            outputTB.Text += Environment.NewLine;
+
+            if ((b - a) < epsilon)
+            {
+                xStar = (a + b) / 2;
+                outputTB.Text += $"x* = {xStar}" + Environment.NewLine;
+                outputTB.Text += Environment.NewLine;
+                return Task.CompletedTask;
+            }
+
+            while (/*!(b - a) <= epsilon)*/k != 10)
+            {
+                k++;
+                outputTB.Text += $"Iteration {k}:" + Environment.NewLine;
+
+                if (MainFunction(alpha) > MainFunction(beta))
+                {
+                    a = alpha;
+                    b = beta;
+                    beta = alpha;
+                    alpha = a + (1 - lambdaGR) * (b - a);
+                    outputTB.Text += $"alpha{k} = {alpha}" + Environment.NewLine;
+                    outputTB.Text += $"beta{k} = {beta}" + Environment.NewLine;
+                }
+                else
+                {
+                    a = alpha;
+                    b = beta;
+                    alpha = beta;
+                    beta = a + lambdaGR * (b - a);
+                    outputTB.Text += $"alpha{k} = {alpha}" + Environment.NewLine;
+                    outputTB.Text += $"beta{k} = {beta}" + Environment.NewLine;
+                }
+
+                outputTB.Text += $"[{a};{b}]" + Environment.NewLine;
+                outputTB.Text += Environment.NewLine;
+            }
+
+            xStar = (a + b) / 2;
+            outputTB.Text += $"~x* = {xStar}" + Environment.NewLine;
+            outputTB.Text += Environment.NewLine;
+            return Task.CompletedTask;
+        }
+
+        private Task FibonacciMethod()
+        {
+            a = 0;
+            b = 1;
+            int n = 0;
+            int k = 1;
+            int FibNMinOne, FibN;
+
+            do
+            {
+                n++;
+                FibNMinOne = Fibonacci(n - 1);
+                FibN = Fibonacci(n);
+                float temp = (float)(b - a) / epsilon;
+                if (FibNMinOne < temp && temp < FibN)
+                    break;
+            }
+            while (true);
+
+            outputTB.Text += $"{n} {Fibonacci(n)}" + Environment.NewLine;
+            alpha = a + ((float)Fibonacci(n-2)/Fibonacci(n))*(b-a);
+            beta = a + ((float)Fibonacci(n-1)/Fibonacci(n+1))*(b-a);
+
+            outputTB.Text += $"alpha{k} = {alpha}" + Environment.NewLine;
+            outputTB.Text += $"beta{k} = {beta}" + Environment.NewLine;
+            outputTB.Text += $"[{a};{b}]" + Environment.NewLine;
+            outputTB.Text += Environment.NewLine;
+
+            while (k != (n - 2))
+            {
+                k++;
+                outputTB.Text += $"Iteration {k}:" + Environment.NewLine;
+
+                outputTB.Text += $"alpha{k} = {alpha}" + Environment.NewLine;
+                outputTB.Text += $"beta{k} = {beta}" + Environment.NewLine;
+
+                switch (MainFunction(alpha) >= MainFunction(beta))
+                {
+                    case true:
+                        {
+                            a = alpha;
+                            alpha = beta;
+                            beta = a + ((float)Fibonacci(n - 1 - k) / Fibonacci(n + 1 - k)) * (b - a);
+                            break;
+                        }
+                    case false:
+                        {
+                            b = beta;
+                            beta = alpha;
+                            alpha = a + ((float)Fibonacci(n - 2 - k) / Fibonacci(n - k)) * (b - a);
+                            break; 
+                        }
+                }
+                outputTB.Text += $"[{a};{b}]" + Environment.NewLine;
+                outputTB.Text += Environment.NewLine;
+            }
+
+            alpha = a;
+            beta = alpha + delta;
+
+            switch (MainFunction(alpha) >= MainFunction(beta))
+            {
+                case true:
+                    {
+                        a = alpha;
+                        break;
+                    }
+                case false:
+                    {
+                        b = beta;
+                        break;
+                    }
+            }
+
+            xStar = (a + b) / 2;
+            outputTB.Text += $"~x* = {xStar}" + Environment.NewLine;
+            outputTB.Text += Environment.NewLine;
+            return Task.CompletedTask;
+        }
+
+        private Task NewtonMethod()
+        {
+            a = 0;
+            b = 1;
+            x = xPrev = 0.5f;
+            int k = 1;
+
+            outputTB.Text += $"x0 = {xPrev}" + Environment.NewLine;
+            outputTB.Text += $"[{a};{b}]" + Environment.NewLine;
+            outputTB.Text += Environment.NewLine;
+
+            x = xPrev + (float)MainFuncFirDerivative(xPrev) / MainFuncSecDerivative(xPrev);
+
+            outputTB.Text += $"x{k} = {x}" + Environment.NewLine;
+
+            outputTB.Text += $"[{a};{b}]" + Environment.NewLine;
+            outputTB.Text += Environment.NewLine;
+
+            while (Math.Abs(x - xPrev) < epsilon)
+            {
+                k++;
+                outputTB.Text += $"Iteration {k}:" + Environment.NewLine;
+
+                x  = xPrev + (float)MainFuncFirDerivative(xPrev)/MainFuncSecDerivative(xPrev);
+
+                outputTB.Text += $"x{k} = {x}" + Environment.NewLine;
+
+                outputTB.Text += $"[{a};{b}]" + Environment.NewLine;
+                outputTB.Text += Environment.NewLine;
+            }
+
+            xStar = x;
+            outputTB.Text += $"~x* = {xStar}" + Environment.NewLine;
+            outputTB.Text += Environment.NewLine;
+            return Task.CompletedTask;
+        }
+        #endregion
+
+        #region Buttons
+        private async void halfDivisionButton_Click(object sender, EventArgs e)
+        {
+            outputTB.Text = "Метод половинного деления" + Environment.NewLine;
+            if (CorrectCheck(false) == -1) return;
+            await HalfDivision();
+        }
+
+        private async void goldenRatioButton_Click(object sender, EventArgs e)
+        {
+            outputTB.Text = "Метод золотого сечения" + Environment.NewLine;
+            if (CorrectCheck(true) == -1) return;
+            await GoldenRatio();
+        }
+
+        private async void FibonacciButton_Click(object sender, EventArgs e)
+        {
+            outputTB.Text = "Метод Фибоначчи" + Environment.NewLine;
+            if (CorrectCheck(false) == -1) return;
+            await FibonacciMethod();
+            
+        }
+
+        private async void NewtonButton_Click(object sender, EventArgs e)
+        {
+            outputTB.Text = "Метод Ньютона" + Environment.NewLine;
+            if (CorrectCheck(true) == -1) return;
+            await NewtonMethod();
+        }
+        #endregion
+    }
+}
